adb54ce2f958cb7f017fb48a818c064a512a886f
aric <aric@3ed01bd8-26fb-0310-9e4c-ca1a4053419f>
2005-11-10 01:56:15 +0000

    test hook needs to unpack data files from egg distribution

--- pygraphviz/tests/test.py
+++ pygraphviz/tests/test.py
@@ -4,18 +4,24 @@ import sys
 import doctest
 import unittest
 
-if sys.version_info[:2] < (2, 4):
-    print "Python version 2.4 or later required for tests (%d.%d detected)." %  sys.version_info[:2]
-    sys.exit(-1)
-try:
-    import pygraphviz
-except:
-    print "Can't import pygraphviz module, not in path"
-    print sys.path
-    raise
+def test_suite():
+    from pkg_resources import resource_filename
+    suite = unittest.TestSuite()
+    doctst = resource_filename(__name__, 'pygraphviz_test.txt')
+    s = doctest.DocFileSuite(doctst,module_relative=False)
+    suite.addTest(s)
+    return suite
+
+if __name__ == "__main__":
+    if sys.version_info[:2] < (2, 4):
+        print "Python version 2.4 or later required for tests (%d.%d detected)." %  sys.version_info[:2]
+        sys.exit(-1)
+    try:
+        import pygraphviz
+    except:
+        print "Can't import pygraphviz module, not in path"
+        print sys.path
+        raise
     
-suite = unittest.TestSuite()
-s = doctest.DocFileSuite('pygraphviz_test.txt',module_relative=False)
-suite.addTest(s)
-runner = unittest.TextTestRunner()
-runner.run(suite)
+    runner = unittest.TextTestRunner()
+    runner.run(test_suite())
