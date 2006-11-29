c5b05cb5c411abfe50c580e2f64604019af52eb5
aric <aric@3ed01bd8-26fb-0310-9e4c-ca1a4053419f>
2006-11-29 22:12:56 +0000

    pygraphviz: adjust test hook, cleaner setup_egg.py, can now run
    >>> python setup_egg.py test
    
    to run the tests

--- pygraphviz/tests/test.py
+++ pygraphviz/tests/test.py
@@ -34,5 +34,5 @@ if __name__ == "__main__":
         print sys.path
         raise
     
-    runner = unittest.TextTestRunner()
-    runner.run(test_suite())
+    run()
+
--- setup_egg.py
+++ setup_egg.py
@@ -2,11 +2,45 @@
 """
 An alternate setup.py script for setuptools.
 
-If you have setuptools and run this as e.g.
-python setup_egg.py bdist_egg
+If you have setuptools and run this as 
+
+>>> python setup_egg.py bdist_egg
+
 you will get a python egg.
+
+Use
+
+>>> python setup_egg.py test
+
+to run the tests.
+
 
 """
 from setuptools import setup, Extension
-execfile('setup.py')
+from setup import *
+
+if __name__ == "__main__":
+
+    setup(
+      name             = name,
+      version          = version,
+      author           = authors['Hagberg'][0],
+      author_email     = authors['Hagberg'][1],
+      description      = description,
+      keywords         = keywords,
+      long_description = long_description,
+      license          = license,
+      platforms        = platforms,
+      url              = url,      
+      download_url     = download_url,
+      packages         = packages,
+      data_files       = data,
+      classifiers      = classifiers,
+      ext_modules      = extension,
+      package_data     = package_data,
+      include_package_data = True,
+      test_suite       = "pygraphviz.tests.test.test_suite", 
+
+      )
+
 
