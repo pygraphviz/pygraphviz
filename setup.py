466ce6214c20fd75b3accb0ec74a0ecf4757c0dd
aric <aric@3ed01bd8-26fb-0310-9e4c-ca1a4053419f>
2007-05-31 17:11:53 +0000

    pygraphviz: Adjust handling of pkg-config and dotneato-config in setup.py to be more robust to all the various graphviz installation types. Addresses #110.

--- setup.py
+++ setup.py
@@ -20,86 +20,116 @@ if sys.argv[-1] == 'setup.py':
     print "To install, run 'python setup.py install'"
     print
 
+library_path=None
+include_path=None
 
-libs=None
-includes=None
 
 # If the setup script couldn't find your graphviz installation you can
 # specify it here by uncommenting these lines or providing your own:
-# You must set both 'libs' and 'includes'
-
-#libs='/packages/lib/graphviz'
-#includes='/packages/include/graphviz'
+# You must set both 'library_path' and 'include_path'
 
 # UNIX, Linux
-#libs='/usr/lib/graphviz'
-#includes='/usr/include/graphviz'
+#library_path='/usr/lib/graphviz'
+#include_path='/usr/include/graphviz'
 
 # UNIX, Linux alternate
-#libs='/usr/local/lib/graphviz'
-#includes='/usr/local/include/graphviz'
+#library_path='/usr/local/lib/graphviz'
+#include_path='/usr/local/include/graphviz'
 
 # Mac OS X (Fink)
-#libs='/sw/lib/graphviz'
-#includes='/sw/include/graphviz'
+#library_path='/sw/lib/graphviz'
+#include_path='/sw/include/graphviz'
 
 # Mac OS X (MacPorts)
-#libs='/opt/local/lib/graphviz'
-#includes='/opt/local/include/graphviz'
+#library_path='/opt/local/lib/graphviz'
+#include_path='/opt/local/include/graphviz'
 
-if libs is None:
+
+def pkg_config():
+    # attempt to find graphviz installation with pkg-config
+    # should work with modern versions of graphviz
+    library_path=None
+    include_path=None
+    try:
+        output,err = \
+                   S.Popen('pkg-config --libs-only-L libagraph',
+                           shell=True, stdin=S.PIPE, stdout=S.PIPE,
+                           close_fds=True).communicate()
+        if output:
+            library_path=output.strip()[2:]
+        output,err = \
+                   S.Popen('pkg-config --cflags-only-I libagraph',
+                           shell=True, stdin=S.PIPE, stdout=S.PIPE,
+                           close_fds=True).communicate()
+        if output:
+            include_path=output.strip()[2:]
+    except:
+        print "Failed to find pkg-config"
+    return include_path,library_path
+
+def dotneato_config():
+    # find graphviz configuration with dotneato-config
+    # works with older versions of graphviz
+    library_path=None
+    include_path=None
+    try:
+        output = S.Popen(['dotneato-config','--ldflags','--cflags'],
+                         stdout=S.PIPE).communicate()[0]
+        if output:
+            include_path,library_path=output.split()
+            library_path=library_path.strip()[2:]
+            include_path=include_path.strip()[2:]
+    except:
+        print "Failed to find dotneato-config"
+    return include_path,library_path
+
+
+
+if library_path is None and include_path is None:
     try:
         import subprocess as S
-        output,err = \
-        S.Popen('pkg-config --libs-only-L --cflags-only-I libagraph',
-                shell=True, stdin=S.PIPE, stdout=S.PIPE,
-                close_fds=True).communicate()
-        if output:
-            includes,libs=output.split()
-            libs=libs.strip()[2:]
-            includes=includes.strip()[2:]
     except ImportError:
         print """-- Missing subprocess package:
         Install subprocess from
         http://effbot.org/downloads/#subprocess
         or set the graphviz paths manually as described below."""
-    if libs is None or libs=='':
-        print "-- Failed to find pkg-config for libagraph"
-        print "   Trying dotneato-config"
-        try:
-            output = S.Popen(['dotneato-config','--ldflags','--cflags'],
-                             stdout=S.PIPE).communicate()[0]
-            if output:
-                includes,libs=output.split()
-                libs=libs.strip()[2:]
-                includes=includes.strip()[2:]
-        except:
-            print "-- Failed to find dotneato-config"
 
+if library_path is None and include_path is None:
+    print "Trying pkg-config"
+    include_path,library_path=pkg_config()
 
-if libs is None or libs=='':
-    libs="/usr/lib/graphviz" # make a guess anyway
-    includes="/usr/includes/graphviz" # make a guess anyway
+if library_path is None and include_path is None:
+    print "Trying dotneato-config"
+    include_path,library_path=dotneato_config()
 
-# sanity check
-try:
-    agraphpath=includes+os.sep+'agraph.h'
-    fh=open(agraphpath,'r')    
-except:
-    print "-- Error: agraph.h include file not found at %s"%agraphpath
-    print "   Incomplete graphviz installation (graphviz-dev missing?)"
+if library_path is None or include_path is None:
+    print 
+    print  """Your graphviz installation could not be found.
+
+Either the graphviz package is missing on incomplete (graphviz-dev missing?).  
+
+If you think your installation is correct you will need to manually
+change the include_path and library_path variables in setup.py to
+point to the correct locations of your graphviz installation.
+
+The current setting of library_path and include_path is:"""
+    print "library_path=%s"%library_path
+    print "include_path=%s"%include_path
     print
-    print  """Your graphviz installation couldn't be found.
-If you think your installation is correct you will 
-need to manually change the includes and libs variables in setup.py
-to point to the correct locations of your graphviz installation.
+    raise OSError,"Error locating graphviz."
 
-The current setting of libs and includes is:"""
-    print "libs=%s"%libs
-    print "includes=%s"%includes
-    print
-    raise "Error locating graphviz."
+print "library_path=%s"%library_path
+print "include_path=%s"%include_path
 
+if len(library_path)>0:
+    library_dirs=[library_path]
+else:
+    library_dirs=None
+
+if len(include_path)>0:
+    include_dirs=[include_path]
+else:
+    include_dirs=None
 
 execfile(os.path.join('pygraphviz','release.py'))
 
@@ -112,8 +142,8 @@ data = [(docdirbase, glob("doc/*.txt")),
         ]
 extension = [Extension("pygraphviz._graphviz",
                       ["pygraphviz/graphviz_wrap.c"],
-                      include_dirs=[includes],
-                      library_dirs=[libs],
+                      include_dirs=include_dirs,
+                      library_dirs=library_dirs,
                       libraries=["agraph","cdt"],
                       )]
 package_data = {'': ['*.txt'],}
