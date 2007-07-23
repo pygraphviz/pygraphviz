377a623f9cd08135fe694a2233aae9265980e081
aric <aric@3ed01bd8-26fb-0310-9e4c-ca1a4053419f>
2007-07-23 17:02:40 +0000

    pygraphviz-0.35

--- pygraphviz/release.py
+++ pygraphviz/release.py
@@ -10,7 +10,7 @@
 
 
 name = 'pygraphviz'
-version = '0.35_svn'
+version = '0.35'
 description = "Python interface to Graphviz"
 long_description = """\
 A Python wrapper for the Graphviz Agraph data structure.
