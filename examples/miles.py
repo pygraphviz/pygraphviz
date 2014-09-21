8c1e3654e0781db476e6fbb518fb4b1a3227b95c
Aric Hagberg <aric.hagberg@gmail.com>
2014-09-21 09:54:28 -0600

    update examples to work with Python3

--- examples/miles.py
+++ examples/miles.py
@@ -6,7 +6,7 @@ and have graphviz "neato" position the edges.
 miles_graph() returns an undirected graph over the 128 US cities from
 the datafile miles_dat.txt.  
 
-This example is described in Section 1.1 in Knuth's book [1,2].
+This example is described in Section 1.1 in Knuth's book [1][2].
 
 The data used in this example is copied from [2].  The filename and
 header have been modified to adhere to the request of the author to
@@ -14,44 +14,26 @@ not corrupt the original source file content and name.
 
 References.
 -----------
-
 [1] Donald E. Knuth,
     "The Stanford GraphBase: A Platform for Combinatorial Computing",
     ACM Press, New York, 1993.
 [2] http://www-cs-faculty.stanford.edu/~knuth/sgb.html
-
-
 """
-
 from __future__ import absolute_import
 from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import division
-
-__author__ = """Aric Hagberg (hagberg@lanl.gov)"""
-#    Copyright (C) 2006-2007 by 
-#    Aric Hagberg <hagberg@lanl.gov>
-#    Dan Schult <dschult@colgate.edu>
-#    Manos Renieris, http://www.cs.brown.edu/~er/
-#    Distributed with BSD license.     
-#    All rights reserved, see LICENSE for details.
-
+import fileinput
+__author__ = """Aric Hagberg (aric.hagberg@gmail.com)"""
 
 def miles_graph():
     """Return a graph from the data in miles_dat.txt.
 
     Edges are made between cities that are less then 300 miles apart.
-
     """
     import math
     import re
-
-    # open file miles_dat.txt.gz (or miles_dat.txt)
-    try:
-        import gzip
-        fh = gzip.open('miles_dat.txt.gz','r')
-    except:
-        fh=open("miles_dat.txt","r")
+    import gzip
 
     G=pgv.AGraph(name='miles_dat')
     G.node_attr['shape']='circle'
@@ -61,16 +43,13 @@ def miles_graph():
     G.graph_attr['outputorder']='edgesfirst'
     G.graph_attr['label']="miles_dat"
     G.graph_attr['ratio']='1.0'
-    G.edge_attr['color']='#AA00FF'
+    G.edge_attr['color']='#1100FF'
     G.edge_attr['style']='setlinewidth(2)'
-#    G.graph_attr['splines']='true'
-#    G.graph_attr['splines']='true'
 
     cities=[]
-    for line in fh.readlines():
+    for line in gzip.open("miles_dat.txt.gz",'rt'):
         if line.startswith("*"): # skip comments
             continue
-
         numfind=re.compile("^\d+") 
 
         if numfind.match(line): # this line is distances
@@ -91,15 +70,14 @@ def miles_graph():
             n.attr['pos']="%f,%f)"%(-(float(x)-7000)/10.0,(float(y)-2000)/10.0)
             # assign node size, in sqrt of 1,000,000's of people 
             d=math.sqrt(float(pop)/1000000.0)
-            n.attr['height']="%s"%d
-            n.attr['width']="%s"%d
+            n.attr['height']="%s"%(d/2)
+            n.attr['width']="%s"%(d/2)
             # assign node color
             n.attr['fillcolor']="#0000%2x"%(int(d*256))
-            # we don't need no labels
+            # empty labels
             n.attr['label']=' '
 
-
-    return G            
+    return G
 
 if __name__ == '__main__':
     import warnings
@@ -110,8 +88,7 @@ if __name__ == '__main__':
 
     G=miles_graph()
     print("Loaded miles_dat.txt containing 128 cities.")
-   
-    G.string()
+
     G.write("miles.dot")
     print("Wrote miles.dot")
     G.draw("miles.png",prog='neato',args='-n2')
