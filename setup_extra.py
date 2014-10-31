f1df09a5ec62d779d5e514d59305dd0858905c3e
Andrea Bedini <andrea@andreabedini.com>
2014-10-31 11:34:06 +1100

    Simplify setup_extra.py

--- setup_extra.py
+++ setup_extra.py
@@ -3,77 +3,61 @@
 """
 Setup helpers for PyGraphviz.
 """
-#    Copyright (C) 2006-2010 by 
+#    Copyright (C) 2006-2010 by
 #    Aric Hagberg <hagberg@lanl.gov>
 #    Dan Schult <dschult@colgate.edu>
 #    Manos Renieris, http://www.cs.brown.edu/~er/
-#    Distributed with BSD license.     
+#    Distributed with BSD license.
 #    All rights reserved, see LICENSE for details.
+
+from __future__ import print_function
 import os
 import sys
+import subprocess as S
 
 def pkg_config():
     # attempt to find graphviz installation with pkg-config
     # should work with modern versions of graphviz
-    try:
-        import subprocess as S
-    except ImportError:
-        print("""-- Missing subprocess package:
-        Install subprocess from
-        http://effbot.org/downloads/#subprocess
-        or set the graphviz paths manually as described below.""")
 
     library_path=None
     include_path=None
     try:
-        output,err = \
-                   S.Popen('pkg-config --libs-only-L libcgraph',
-                           shell=True, stdin=S.PIPE, stdout=S.PIPE,
-                           close_fds=True).communicate()
-        output = output.decode(sys.stdout.encoding)
-        if output:
-            library_path=output.strip()[2:]
-        output,err = \
-                   S.Popen('pkg-config --cflags-only-I libcgraph',
-                           shell=True, stdin=S.PIPE, stdout=S.PIPE,
-                           close_fds=True).communicate()
-        output = output.decode(sys.stdout.encoding)
-        if output:
-            include_path=output.strip()[2:]
-    except:
+        output = S.check_output(['pkg-config', '--libs-only-L', 'libcgraph'])
+        library_path = output.strip()[2:]
+
+        output = S.check_output(['pkg-config', '--cflags-only-I',
+            'libcgraph'])
+        include_path = output.strip()[2:]
+    except S.CalledProcessError:
         print("Failed to find pkg-config")
-    return include_path,library_path
+
+    return include_path, library_path
 
 def dotneato_config():
     # find graphviz configuration with dotneato-config
     # works with older versions of graphviz
-    # attempt to find graphviz installation with pkg-config
-    # should work with modern versions of graphviz
+
+    library_path = None
+    include_path = None
     try:
-        import subprocess as S
-    except ImportError:
-        print("""-- Missing subprocess package:
-        Install subprocess from
-        http://effbot.org/downloads/#subprocess
-        or set the graphviz paths manually as described below.""")
-    library_path=None
-    include_path=None
+        output = S.check_output(['dotneato-config', '--ldflags', '--cflags'])
+        include_path, library_path = output.split()
+        library_path = library_path.strip()[2:]
+        include_path = include_path.strip()[2:]
+    except S.CalledProcessError:
+        print("Failed to find dotneato-config")
+        # fall through and test the other syntax
+    else:
+        return include_path, library_path
+
     try:
-        output = S.Popen(['dotneato-config','--ldflags','--cflags'],
-                         stdout=S.PIPE).communicate()[0]
-        if output:
-            include_path,library_path=output.split()
-            library_path=library_path.strip()[2:]
-            include_path=include_path.strip()[2:]
-        else:
-            output = S.Popen(['dotneato-config','--libs','--cflags'],
-                         stdout=S.PIPE).communicate()[0]
-            if output:
-                include_path,library_path=output.split('\n',1)
-                library_path=library_path.strip()[2:]
-                include_path=include_path.strip()[2:]
+        output = S.check_output(['dotneato-config', '--libs', '--cflags'])
+        include_path, library_path = output.split('\n', 1)
+        library_path = library_path.strip()[2:]
+        include_path = include_path.strip()[2:]
     except:
         print("Failed to find dotneato-config")
-    return include_path,library_path
 
+    # nothing else to try, just return
+    return include_path, library_path
 
