ecb4f0379f401e9984a580b46e7ba6afc07d60c2
Jason Madden <jamadden@gmail.com>
2015-09-26 09:27:43 -0500

    Enable the read/write test for code coverage purposes, taking care to deal with the filehandles in a way that should work under Win32.

--- pygraphviz/tests/test_readwrite.py
+++ pygraphviz/tests/test_readwrite.py
@@ -1,17 +1,22 @@
 # -*- coding: utf-8 -*-
-from nose.tools import *
+from nose.tools import assert_equal
+from nose.tools import assert_true
+from nose.tools import assert_false
 import pygraphviz as pgv
+import os
+import tempfile
 
 def test_readwrite():
     A = pgv.AGraph(name='test graph')
     A.add_path([1,2,3,4,5,6,7,8,9,10])
-
-#FIXME
-# >>> (fd,fname)=tempfile.mkstemp()
-    # A.write(fname)
-    # A.read(fname)
-    # assert_equal(B, AGraph(fname))
-    # assert_true(B == A)
-    # assert_false(B is A)
-# >>> os.close(fd)
-# >>> os.unlink(fname)
+    the_file = tempfile.NamedTemporaryFile(delete=False)
+    fname = the_file.name
+    # Make sure it can be opened for writing again on Win32
+    the_file.close()
+    # Pass a string to trigger the code paths that close the newly created file handle
+    A.write(fname)
+    B = pgv.AGraph(fname)
+    assert_equal(A, B)
+    assert_true(B == A)
+    assert_false(B is A)
+    os.unlink(fname)
