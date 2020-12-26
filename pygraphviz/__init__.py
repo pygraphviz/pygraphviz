de5cc9ea909573ea8f2383b644fdf16c17e27995
Jarrod Millman <jarrod.millman@gmail.com>
2020-12-25 22:13:30 -0800

    Simplify version information

--- pygraphviz/__init__.py
+++ pygraphviz/__init__.py
@@ -1,36 +1,14 @@
 """
+PyGraphviz
+==========
+
 A Python wrapper for the graphviz Agraph data structure.
 
-Quick example::
-
->>> from pygraphviz import *
->>> G=AGraph()
->>> G.add_node('a')
->>> G.add_edge('b','c')
->>> print G  # doctest: +SKIP 
-strict graph {
-    a;
-    b -- c;
-}
-<BLANKLINE>
-
+See https://pygraphviz.github.io for complete documentation.
 See pygraphviz.AGraph for detailed documentation.
-
 """
 
-# Release data
-from . import release
-
-__date__ = release.date
-__version__ = release.version
-
-__author__ = (
-    f"{release.authors['Hagberg'][0]} <{release.authors['Hagberg'][1]}>\n"
-    f"{release.authors['Schult'][0]} <{release.authors['Schult'][1]}>\n"
-    f"{release.authors['Renieris'][0]} <{release.authors['Renieris'][1]}>"
-)
-
-__license__ = release.license
+__version__ = "1.7rc1.dev0"
 
 from .agraph import AGraph, Node, Edge, Attribute, ItemAttribute, DotError
 
--- setup.py
+++ setup.py
@@ -1,44 +1,74 @@
-"""
-Setup script for PyGraphviz
-"""
-
 from glob import glob
-
 import os
-from setuptools import setup, Extension
 import sys
-
-if os.path.exists('MANIFEST'): os.remove('MANIFEST')
-
-if sys.argv[-1] == 'setup.py':
-    print("To install, run 'python setup.py install'")
-    print()
+from setuptools import setup, Extension
 
 if sys.version_info[:2] < (3, 7):
-    print("PyGraphviz requires Python version 3.7 or later (%d.%d detected)." %
-          sys.version_info[:2])
+    error = (
+        "PyGraphviz requires Python version 3.7 or later (%d.%d detected)."
+        % sys.version_info[:2]
+    )
+    sys.stderr.write(error + "\n")
     sys.exit(-1)
 
-# Write the version information.
-sys.path.insert(0, 'pygraphviz')
-import release
-release.write_versionfile()
-sys.path.pop(0)
+
+name = "pygraphviz"
+description = "Python interface to Graphviz"
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+license = "BSD"
+authors = {
+    "Hagberg": ("Aric Hagberg", "aric.hagberg@gmail.com"),
+    "Schult": ("Dan Schult", "dschult@colgate.edu"),
+    "Renieris": ("Manos Renieris", ""),
+}
+url = "http://pygraphviz.github.io"
+download_url = "https://pypi.python.org/pypi/pygraphviz"
+platforms = ["Linux", "Mac OSX", "Microsoft :: Windows"]
+keywords = ["Networks", "Graph Visualization", "network", "graph", "graph drawing"]
+classifiers = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: BSD License",
+    "Programming Language :: C",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Programming Language :: Python :: Implementation :: PyPy",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: Scientific/Engineering :: Information Analysis",
+    "Topic :: Scientific/Engineering :: Mathematics",
+    "Topic :: Scientific/Engineering :: Visualization",
+]
+
+with open("pygraphviz/__init__.py") as fid:
+    for line in fid:
+        if line.startswith("__version__"):
+            version = line.strip().split()[-1][1:-1]
+            break
 
 packages = ["pygraphviz", "pygraphviz.tests"]
-docdirbase = 'share/doc/pygraphviz-%s' % release.version
+docdirbase = "share/doc/pygraphviz-%s" % version
 data = [
     (docdirbase, glob("*.txt")),
-    (os.path.join(docdirbase, 'examples'), glob("examples/*.py")),
-    (os.path.join(docdirbase, 'examples'), glob("examples/*.dat")),
-    (os.path.join(docdirbase, 'examples'), glob("examples/*.dat.gz")),
+    (os.path.join(docdirbase, "examples"), glob("examples/*.py")),
+    (os.path.join(docdirbase, "examples"), glob("examples/*.dat")),
+    (os.path.join(docdirbase, "examples"), glob("examples/*.dat.gz")),
 ]
-package_data = {'': ['*.txt'], }
+package_data = {
+    "": ["*.txt"],
+}
 
 if __name__ == "__main__":
     define_macros = []
     if sys.platform == "win32":
-        define_macros = define_macros.append(('GVDLL', None))
+        define_macros = define_macros.append(("GVDLL", None))
 
     extension = [
         Extension(
@@ -53,28 +83,28 @@ if __name__ == "__main__":
             # link list, having only cdt and preventing the module from being loaded with
             # undefined symbol errors. seen under PyPy on Linux.)
             libraries=["cdt", "cgraph", "gvc"],
-            define_macros=define_macros
+            define_macros=define_macros,
         )
     ]
 
     setup(
-        name=release.name,
-        version=release.version,
-        author=release.authors['Hagberg'][0],
-        author_email=release.authors['Hagberg'][1],
-        description=release.description,
-        keywords=release.keywords,
-        long_description=release.long_description,
-        license=release.license,
-        platforms=release.platforms,
-        url=release.url,
-        download_url=release.download_url,
-        classifiers=release.classifiers,
+        name=name,
+        version=version,
+        author=authors["Hagberg"][0],
+        author_email=authors["Hagberg"][1],
+        description=description,
+        keywords=keywords,
+        long_description=long_description,
+        license=license,
+        platforms=platforms,
+        url=url,
+        download_url=download_url,
+        classifiers=classifiers,
         packages=packages,
         data_files=data,
         ext_modules=extension,
         package_data=package_data,
-        include_package_data = True,
-        python_requires='>=3.7',
-        tests_require=['pytest'],
+        include_package_data=True,
+        python_requires=">=3.7",
+        tests_require=["pytest"],
     )
