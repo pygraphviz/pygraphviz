3c7423169ff0076a7ba31e1bdf3ad8b61d431476
Jarrod Millman <jarrod.millman@gmail.com>
2021-01-04 10:47:20 -0800

    Designate 1.7rc1 release

--- pygraphviz/__init__.py
+++ pygraphviz/__init__.py
@@ -8,7 +8,7 @@ See https://pygraphviz.github.io for complete documentation.
 See pygraphviz.AGraph for detailed documentation.
 """
 
-__version__ = "1.7rc1.dev0"
+__version__ = "1.7rc1"
 
 from .agraph import AGraph, Node, Edge, Attribute, ItemAttribute, DotError
 
