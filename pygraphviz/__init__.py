c7a1024ad0ad2e97b88afd5aa6d4941b3464d183
Jarrod Millman <jarrod.millman@gmail.com>
2021-01-16 20:42:49 -0800

    Handle change in DLL dependency resolver

--- pygraphviz/__init__.py
+++ pygraphviz/__init__.py
@@ -8,6 +8,17 @@ See https://pygraphviz.github.io for complete documentation.
 See pygraphviz.AGraph for detailed documentation.
 """
 
+import sys
+
+# https://docs.python.org/3/whatsnew/3.8.html#bpo-36085-whatsnew
+if sys.version_info >= (3, 8, 0) and sys.platform == "win32":
+    import os
+
+    for path in os.environ["PATH"].split(os.pathsep):
+        if "graphviz" in path.lower() and os.path.exists(path):
+            os.add_dll_directory(path)
+
+
 __version__ = "1.7rc2.dev0"
 
 from .agraph import AGraph, Node, Edge, Attribute, ItemAttribute, DotError
@@ -18,3 +29,5 @@ from . import testing
 
 # Per contract with Sphinx-Gallery, this method must be available at top level
 from pygraphviz.scraper import _get_sg_image_scraper
+
+del sys
