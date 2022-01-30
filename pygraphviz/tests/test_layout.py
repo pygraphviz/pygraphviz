37ef89606ac0e8202e4213d9f4f418a946805dc6
Ross Barnowski <rossbar@berkeley.edu>
2022-01-29 19:16:36 -0800

    Update test with new layout progs.

--- pygraphviz/tests/test_layout.py
+++ pygraphviz/tests/test_layout.py
@@ -22,33 +22,32 @@ def test_layout_defaults():
     # print("Pos after",[n.attr["pos"] for n in A.nodes()])
 
 
-def test_layout_prog_arg():
+@pytest.mark.parametrize(
+    "prog", ("neato", "dot", "twopi", "circo", "fdp", "osage", "patchwork", "sfdp"),
+)
+def test_layout_prog_arg(prog):
     A = pgv.AGraph(name="test graph")
     A.add_path([1, 2, 3, 4])
-    assert [n.attr["pos"] is None for n in A.nodes()] == [True] * 4
-    A.layout(prog="dot")
-    assert [n.attr["pos"] is not None for n in A.nodes()] == [True] * 4
+
+    # No node position before layout
+    assert all(n.attr["pos"] is None for n in A.nodes())
+
+    # All nodes should have a position after layout
+    A.layout(prog=prog)
+    assert all(n.attr["pos"] is not None for n in A.nodes())
+
+    # Node positions should be different for each of the layouts
+    pos = [n.attr["pos"] for n in A.nodes()]
+    A.layout(prog="dot")  # Use dot layout as reference
     dot_pos = [n.attr["pos"] for n in A.nodes()]
+    assert pos == dot_pos if prog == "dot" else pos != dot_pos
 
-    A.layout(prog="dot")
-    result = [n.attr["pos"] for n in A.nodes()]
-    assert result == dot_pos
 
-    A.layout(prog="twopi")
-    result = [n.attr["pos"] for n in A.nodes()]
-    assert result != dot_pos
-
-    A.layout(prog="neato")
-    result = [n.attr["pos"] for n in A.nodes()]
-    assert result != dot_pos
-
-    A.layout(prog="circo")
-    result = [n.attr["pos"] for n in A.nodes()]
-    assert result != dot_pos
-
-    A.layout(prog="fdp")
-    result = [n.attr["pos"] for n in A.nodes()]
-    assert result != dot_pos
+def test_bad_prog_arg_raises():
+    A = pgv.AGraph()
+    A.add_path([1, 2, 3, 4])
+    with pytest.raises(ValueError, match="Program.*not found in path"):
+        A.layout(prog="not-a-valid-layout")
 
 
 class TestExperimentalGraphvizLibInterface:
